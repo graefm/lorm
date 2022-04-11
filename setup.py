19b94507711bc72c33253c0837cb9790beec3b5d
Manuel Gräf <manuel.graef@tu-berlin.de>
2022-04-11 11:21:48 +0200

    Move library code to 'src'. Update setup and license file.

--- setup.py
+++ setup.py
@@ -6,58 +6,62 @@ import numpy
 
 package_name = 'lorm'
 nfft_name = 'nfft'
-setup_dir = dir = os.path.dirname(os.path.abspath(__file__))
+setup_dir = os.path.dirname(os.path.abspath(__file__)) + '/src'
+print(setup_dir)
 package_dir = os.path.join(setup_dir, package_name)
 nfft_dir = os.path.join(setup_dir, nfft_name)
-#nfft_libs = 'nfft3 fftw3'.split()
 nfft_libs = 'nfft3_threads fftw3_threads'.split()
 
 ext_modules = []
 ext_modules.append(Extension(
-            name=package_name+'.cmanif',
-            sources=[os.path.join(package_dir, 'cmanif.pyx')],
-            extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
-            ))
+    name=package_name + '.cmanif',
+    sources=[os.path.join(package_dir, 'cmanif.pyx')],
+    extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
+))
 ext_modules.append(Extension(
-            name=package_name+'.cfuncs',
-            sources=[os.path.join(package_dir, 'cfuncs.pyx')],
-            extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
-            ))
+    name=package_name + '.cfuncs',
+    sources=[os.path.join(package_dir, 'cfuncs.pyx')],
+    extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
+))
 ext_modules.append(Extension(
-            name=nfft_name+'.cnfft',
-            sources=[os.path.join(nfft_dir, 'cnfft.pyx')],
-            libraries=nfft_libs,
-            library_dirs=[],
-            include_dirs=[numpy.get_include()],
-            extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
-            ))
+    name=nfft_name + '.cnfft',
+    sources=[os.path.join(nfft_dir, 'cnfft.pyx')],
+    libraries=nfft_libs,
+    library_dirs=[],
+    include_dirs=[numpy.get_include()],
+    extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
+))
 ext_modules.append(Extension(
-            name=nfft_name+'.cnfsft',
-            sources=[os.path.join(nfft_dir, 'cnfsft.pyx')],
-            libraries=nfft_libs,
-            library_dirs=[],
-            include_dirs=[numpy.get_include()],
-            extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
-            ))
+    name=nfft_name + '.cnfsft',
+    sources=[os.path.join(nfft_dir, 'cnfsft.pyx')],
+    libraries=nfft_libs,
+    library_dirs=[],
+    include_dirs=[numpy.get_include()],
+    extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
+))
 ext_modules.append(Extension(
-            name=nfft_name+'.cnfsoft',
-            sources=[os.path.join(nfft_dir, 'cnfsoft.pyx')],
-            libraries=nfft_libs,
-            library_dirs=[],
-            include_dirs=[numpy.get_include()],
-            extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
-            ))
+    name=nfft_name + '.cnfsoft',
+    sources=[os.path.join(nfft_dir, 'cnfsoft.pyx')],
+    libraries=nfft_libs,
+    library_dirs=[],
+    include_dirs=[numpy.get_include()],
+    extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
+))
 ext_modules.append(Extension(
-            name=nfft_name+'.cnfdsft',
-            sources=[os.path.join(nfft_dir, 'cnfdsft.pyx')],
-            libraries=nfft_libs,
-            library_dirs=[],
-            include_dirs=[numpy.get_include()],
-            extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
-            ))
+    name=nfft_name + '.cnfdsft',
+    sources=[os.path.join(nfft_dir, 'cnfdsft.pyx')],
+    libraries=nfft_libs,
+    library_dirs=[],
+    include_dirs=[numpy.get_include()],
+    extra_compile_args='-O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math'.split()
+))
 
 setup(name="lorm",
-      version="0.0.1",
-      packages=["lorm","nfft","disc"],
-      ext_modules=cythonize(ext_modules)
+      description="Library for Optimization on Riemannian Manifolds",
+      author="Manuel Gräf",
+      version="0.0.2",
+      package_dir={"": "src"},
+      packages=["lorm", "nfft", "disc"],
+      ext_modules=cythonize(ext_modules),
+      license="MIT"
       )
